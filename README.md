c43db81a5c02bc314eb5aa30ae507c7faaaf8d63
meysam <meisam.design.ph@gmail.com>
2021-10-04 00:13:44 +0330

    improve code and add screenshot

--- README.md
+++ README.md
@@ -10,11 +10,11 @@ Users should be able to:
 
 ### Screenshot
 
-![](./screenshot.jpg)
+![](./screenshot.png)
 
 Add a screenshot of your solution. The easiest way to do this is to use Firefox to view your project, right-click the page and select "Take a Screenshot". You can choose either a full-height screenshot or a cropped one based on how long the page is. If it's very long, it might be best to crop it.
 
-Alternatively, you can use a tool like [FireShot](https://getfireshot.com/) to take the screenshot. FireShot has a free option, so you don't need to purchase it. 
+Alternatively, you can use a tool like [FireShot](https://getfireshot.com/) to take the screenshot. FireShot has a free option, so you don't need to purchase it.
 
 Then crop/optimize/edit your image however you like, add it to your project, and update the file path in the image above.
 
@@ -25,7 +25,6 @@ Then crop/optimize/edit your image however you like, add it to your project, and
 - Solution URL: [Solution URL](https://github.com/meysamminoo/tip-calculator-app)
 - Live Site URL: [Live site URL](https://meysamminoo.github.io/tip-calculator-app/)
 
-
 ### What I learned
 
 Use this section to recap over some of your major learnings while working through this project. Writing these out and providing code samples of areas you want to highlight is a great way to reinforce your own knowledge.
@@ -34,4 +33,3 @@ Use this section to recap over some of your major learnings while working throug
 
 - [Example resource 1](https://www.example.com) - This helped me for XYZ reason. I really liked this pattern and will use it going forward.
 - [Example resource 2](https://www.example.com) - This is an amazing article which helped me finally understand XYZ. I'd recommend it to anyone still learning this concept.
-
--- index.js
+++ index.js
@@ -1,66 +1,94 @@
-// get element
-let tip = document.querySelectorAll(".grid li");
+// get input => bill , select tip, custom tip , number of people
 let bill = document.getElementById("bill");
-let person = document.getElementById("person")
-let customTip = document.getElementById("custom-tip")
+let tip = document.querySelectorAll(".grid li");
+let person = document.getElementById("person");
+let customTip = document.getElementById("custom-tip");
+//  get output => tip amount, total
 let tipAmountFinal = document.getElementById("tip-amount");
 let tipTotalFinal = document.getElementById("total");
+// get button reset
 const btn = document.getElementById("btn");
+// get box error => zero number of people
 let messageError = document.querySelector(".error-message");
+// variable - select tip
 let tipAmount;
 
+selectTip();
+// calculated tip amount and total tip
 function calculator() {
   showErrorMessega();
-  if( customTip.value == ""){
-    for (iterator of tip) {
-      iterator.addEventListener("click", function(){
-        tipAmount=this;
-        customTip.value = Number(this.value);
-        activeButton();
-        calculatorTip();
-      })
-    }
-  } else{
+  if( customTip.value === ""){
     calculatorTip();
-  }
-  
-}
-
-function activeButton(){
-  if(!tipAmount.classList.contains("active")){
-    tipAmount.classList.add("active");
-  } else {
-    tipAmount.classList.remove("active");
+  } else{
+    calculatorCustomTip();
   }
 }
 
-// calculator tip for person and total tip
+//  selected tip and active one tip
+function selectTip(){
+  for (let i= 0; i < tip.length; i++) {
+    tip[i].addEventListener("click", function(){
+      customTip.value = "";
+      tipAmount=this.value;
+      let setClasses = !this.classList.contains('active');
+      setClass(tip, 'active', 'remove');
+      if (setClasses) {
+        this.classList.add("active");
+      }
+      calculator()
+    })
+  }
+}
+// calculator custom tip
+function calculatorCustomTip(){
+  for (let i= 0; i < tip.length; i++) {
+    tip[i].classList.remove("active");
+  }
+  if (bill.value > 0 && person.value > 0 && customTip.value > 0) {
+    tipAmountFinal.innerText = "$" + (parseFloat(bill.value) * parseFloat(customTip.value) / 100).toPrecision(3);
+    tipTotalFinal.innerText = "$" + (parseFloat(bill.value) * parseFloat(customTip.value)/100 / parseFloat(person.value)).toPrecision(3);
+    btnReset();
+  }
+}
+// calculator selected tip
 function calculatorTip(){
-  tipAmountFinal.innerText = "$" + (Number(bill.value) * Number(customTip.value) / 100).toFixed(2);;
-  tipTotalFinal.innerText = "$" + (Number(bill.value) * Number(customTip.value)/100 / Number(person.value)).toFixed(2);
-  btnReset()
+  if (bill.value > 0 && person.value > 0 && tipAmount > 0) {
+    tipAmountFinal.innerText = "$" + (parseFloat(bill.value) * tipAmount / 100).toPrecision(3);
+    tipTotalFinal.innerText = "$" + (parseFloat(bill.value) * tipAmount/100 / parseFloat(person.value)).toPrecision(3);
+    btnReset();
+  }
 }
 
-// reset calculator
+// reset input after click button reset
 function btnReset(){
   btn.classList.remove("btn-disable");
   btn.classList.add("btn-hover");
   btn.addEventListener("click",function(){
     bill.value = "";
     customTip.value = "";
-    person.value = 1;
-    tipAmountFinal.innerText = "$0.00"
-    tipTotalFinal.innerText = "$0.00"
+    person.value = "";
+    tipAmountFinal.innerText = "$0.00";
+    tipTotalFinal.innerText = "$0.00";
     btn.classList.add("btn-disable");
     btn.classList.remove("btn-hover");
   })
 }
 
-// show error message
+// show error message and handel preview
 function showErrorMessega(){
-  if (person.value != 0 || person.value==""){
+  if (person.value != 0 ){
     messageError.style.display = "none";
   } else {
     messageError.style.display = "block";
+  }
+  if (bill.value === ""){
+    tipAmountFinal.innerText = "$0.00";
+    tipTotalFinal.innerText = "$0.00";
+  }
+}
+//  function for active one selected tip
+function setClass(els, className, fnName) {
+  for (let index = 0; index < els.length; index++) {
+    els[index].classList[fnName](className);
   }
 }
